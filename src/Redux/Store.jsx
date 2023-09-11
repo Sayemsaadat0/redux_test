4b706c56f646fda59f72c4183bbe0365c01e6ba1
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-09-11 19:05:49 +0600

    fresh file

--- src/Redux/Store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counter from './features/counter/counterSlicer'
-
-
-const store = configureStore({
-    reducer : {counter},
-})
-
-export default store
